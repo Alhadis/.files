b54d2e9627deff8063489fc291571120d9d01948
Alhadis <gardnerjohng@gmail.com>
2015-10-24 04:35:22 +1100

    Add a very helpful suite of aliases and functions
    Credit and kind thanks go to:
    
    *  https://github.com/mathiasbynens/dotfiles
    *  https://github.com/paulirish/dotfiles

--- aliases.sh
+++ aliases.sh
@@ -70,3 +70,23 @@ alias unquarantine='find .  -print0 -type f -o -type d | xargs -0 xattr -d com.a
 alias hooks-on='perl -pi -e '"'"'s/(static\s+\$trace_(?:filters|actions)\s*=\s*)\d/${1}1/g'"'"' hooktracer.php'
 alias hooks-off='perl -pi -e '"'"'s/(static\s+\$trace_(?:filters|actions)\s*=\s*)\d/${1}0/g'"'"' hooktracer.php'
 alias hooks-reset='> ~/Documents/apache.log'
+
+
+
+
+#==============================================================================
+#	LOOTED FROM BELGIAN FLY-KICK GUY:
+#	*	https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
+#==============================================================================
+
+# IP addresses
+alias ip='dig +short myip.opendns.com @resolver1.opendns.com'
+alias localip='ipconfig getifaddr en0'
+
+# Clean up LaunchServices to remove duplicates in the "Open With" menu
+alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+
+# Empty the Trash on all mounted volumes and the main HDD.
+# Also, clear Apple's System Logs to improve shell startup speed.
+# Finally, clear download history from quarantine. https://mths.be/bum
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
--- functions.sh
+++ functions.sh
@@ -9,3 +9,89 @@ fit(){
 		end tell';
 	};
 }
+
+
+
+
+#==============================================================================
+#	FOLLOWING FUNCTIONS ALL SHAMELESSLY PINCHED FROM THESE LOVELY CHAPS:
+#
+#	*	https://github.com/mathiasbynens/dotfiles
+#	*	https://github.com/paulirish/dotfiles
+#==============================================================================
+
+
+# "Find" shorthand
+function f(){
+	find . -name "$1" 2>&1 | grep -v 'Permission denied'
+}
+
+
+# Change directory to whatever's in the forefront Finder window
+# Mnemonic: "cd Finder"
+function cdf(){
+	cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
+}
+
+
+# Use Git's coloured diff
+function diff(){
+	git diff --no-index --color-words "$@";
+}
+
+
+# Get a character's Unicode code point
+function codepoint(){
+	perl -e "use utf8; print sprintf('U+%04X', ord(\"$@\"))";
+
+	# Print a newline unless we're piping the output to another program
+	if [ -t 1 ]; then
+		echo ""; # newline
+	fi;
+}
+
+
+# Decode \x{ABCD}-style Unicode escape sequences
+function unidecode(){
+	perl -e "binmode(STDOUT, ':utf8'); print \"$@\"";
+	# Print a newline unless we're piping the output to another program
+	if [ -t 1 ]; then
+		echo ""; # newline
+	fi;
+}
+
+
+# Look-up a domain or a URL in whois
+function whois(){
+	local domain=$(echo "$1" | awk -F/ '{print $3}')
+	if [ -z $domain ] ; then
+		domain=$1
+	fi
+	echo "Getting whois record for: $domain …"
+	/usr/bin/whois -h whois.internic.net $domain | sed '/NOTICE:/q'
+}
+
+
+# Simple calculator
+function calc(){
+	local result="";
+	result="$(printf "scale=10;$*\n" | bc --mathlib | tr -d '\\\n')";
+	if [[ "$result" == *.* ]]; then
+		# Improve the output for decimal numbers
+		printf "$result" |
+		sed -e 's/^\./0./'         # add "0" for cases like ".5"  \
+		    -e 's/^-\./-0./'       # add "0" for cases like "-.5" \
+		    -e 's/0*$//;s/\.$//';  # remove trailing zeros
+	else
+		printf "$result";
+	fi;
+	printf "\n";
+}
+
+
+# Check who's been using the laptop's iSight camera
+camerausedby(){
+	echo -e "Checking to see who is using the iSight camera… \xF0\x9F\x93\xB7"
+	usedby=$(lsof | grep -w "AppleCamera\|USBVDC\|iSight" | awk '{printf $2"\n"}' | xargs ps)
+	echo -e "Recent camera uses:\n$usedby"
+}
