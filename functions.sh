d2e37dd1dacc81c2a0c793eaaa6800cc889ad79b
Alhadis <gardnerjohng@gmail.com>
2015-10-25 05:15:41 +1100

    Fix overzealous zero-removal applied by 967985b34
    Probably should've tested that with "calc 30 + 40" *before* committing…

--- functions.sh
+++ functions.sh
@@ -59,7 +59,7 @@ function calc(){
 	local result=$(printf "$*\n" | tr x '*' | sed -re 's/[A-Za-z]+/ /g' | bc -l | tr -d '\\\n')
 	
 	# Drop trailing zeroes
-	printf "$result" | sed -re 's/(\.0+|0+)$//g;' | pbcopy;
+	printf "$result" | perl -pe 's/(\.[1-9]+)0+$|(\.0+$)/$1/g' | pbcopy;
 	
 	# Copy to STDERR
 	pbpaste;
