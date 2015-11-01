d19d5ace0c2fe63b5461a41c507536d91752a8e9
Alhadis <gardnerjohng@gmail.com>
2015-11-01 21:27:19 +1100

    Store path to PHP's loaded configuration file

--- paths.sh
+++ paths.sh
@@ -7,3 +7,4 @@ export SNIP=~/Labs/Snippets
 # Stuff I can never seem to memorise
 export APACHE_DIR=/private/etc/apache2
 export APACHE_LOGS=/private/var/log/apache2
+export PHP_INI=$(php -i | grep 'Loaded Configuration File' | sed -E 's/\s*=>\s*/:/g' | cut -d : -f 2)
