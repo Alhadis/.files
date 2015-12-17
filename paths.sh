6185e488db5b41a3c03fb8437216aaf6267ae9ba
Alhadis <gardnerjohng@gmail.com>
2015-12-17 22:33:27 +1100

    Add aliases for Snippets directory
    I keep forgetting whether I named my Snippets path $SNIPS or just $SNIP.

--- paths.sh
+++ paths.sh
@@ -8,3 +8,7 @@ export SNIP=~/Labs/Snippets
 export APACHE_DIR=/private/etc/apache2
 export APACHE_LOGS=/private/var/log/apache2
 export PHP_INI=$(php -i | grep 'Loaded Configuration File' | sed -E 's/\s*=>\s*/:/g' | cut -d : -f 2)
+
+# Stuff I can only memorise halfway
+export SNIPS=$SNIP
+export SNIPPETS=$SNIP
