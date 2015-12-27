b674c85e08e7858d464c84b63fd1077d2f0a9f6b
Alhadis <gardnerjohng@gmail.com>
2015-12-28 01:46:05 +1100

    Copy iTunes library backup to hard-drive if present

--- bin/itunes-library-backup.sh
+++ bin/itunes-library-backup.sh
@@ -5,18 +5,25 @@ IFS=
 cd ~/Music/iTunes
 
 files=('iTunes Library Extras.itdb' 'iTunes Library Genius.itdb' 'iTunes Library.itl' 'iTunes Music Library.xml')
-tmpdir='MacBook iTunes Library Backup II'
+backup='MacBook iTunes Library Backup II'
+dropbox=~/Dropbox/Backups/
 
-mkdir $tmpdir
+mkdir $backup
 for i in ${files[@]}; do
-	cp $i $tmpdir/$i
+	cp $i $backup/$i
 done;
 
 
-zip -r "$tmpdir.zip" $tmpdir -x "*.DS_Store"
-mv $tmpdir.zip ~/Dropbox/Backups/
-rm -rf $tmpdir
+zip -r "$backup.zip" $backup -x "*.DS_Store"
+mv $backup.zip $dropbox
+rm -rf $backup
 
 
 # Upload with Dropbox
 open /Applications/Dropbox.app
+
+
+# Copy to portable hard drive if it's plugged in
+[ -d /Volumes/"Amra Helion" ] && {
+	cp "$dropbox/$backup.zip" "/Volumes/Amra Helion/MacBook/";
+};
