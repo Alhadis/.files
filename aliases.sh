d532a13d3b9474b58bfdc0ab5467d92f10b099b3
Alhadis <gardnerjohng@gmail.com>
2016-04-07 20:52:49 +1000

    Add a one-liner to update/upgrade Homebrew formulae
    The alias's name is a contracted form of "brew update/upgrade".

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,7 @@
 #   COMMON SHORTHAND
 #==============================================================================
 alias bc='bc -l'
+alias bup='brew update | grep -v "Already up-to-date"; [ "$(brew outdated)" ] && brew upgrade --all && brew cleanup;'
 alias c='calc'
 alias fs='pbpaste | filesafe | pbcopy'
 alias G:='cd ~/VirtualBox/Shared'
