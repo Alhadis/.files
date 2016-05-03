9ba2ea611dd49a8be5004a7fb15d6ed04b3e0d80
Alhadis <gardnerjohng@gmail.com>
2016-05-04 03:25:38 +1000

    Include local Linguist language listing when updating

--- functions.sh
+++ functions.sh
@@ -45,6 +45,11 @@ update(){
 	gem update --system;
 	gem update;
 	
+	# Update local language listing
+	marker "Updating Linguist language listing"
+	local langfile="https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml"
+	wget $langfile -O ~/Documents/"GitHub Languages.yml"
+	
 	# Update forks/copies of other people's repos
 	marker "Updating forked repositories"
 	~/Forks/update
