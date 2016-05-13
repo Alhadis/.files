e9a815d4d33ca87a44f1910e1498b73d9d960648
Alhadis <gardnerjohng@gmail.com>
2016-05-14 01:41:52 +1000

    Add an Emacs initialisation file

---
+++ .emacs.el
@@ -0,0 +1,32 @@
+;; UTF-8 support
+;; (set-language-environment "UTF-8")
+(setenv "LANG" "en_AU.UTF-8")
+(setenv "LC_ALL" "en_AU.UTF-8")
+
+
+;;; Function to load all ".el" files in ~/.emacs.d/config
+(defun load-directory (directory)
+  "Recursively load all Emacs Lisp files in a directory."
+  (dolist (element (directory-files-and-attributes directory nil nil nil))
+    (let* ((path (car element))
+           (fullpath (concat directory "/" path))
+           (isdir (car (cdr element)))
+           (ignore-dir (or (string= path ".") (string= path ".."))))
+      (cond
+       ((and (eq isdir t) (not ignore-dir))
+        (load-directory fullpath))
+       ((and (eq isdir nil) (string= (substring path -3) ".el"))
+        (load (file-name-sans-extension fullpath)))))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
