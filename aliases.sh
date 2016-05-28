2857e9526e278ad836fcda00121751305209ab20
Alhadis <gardnerjohng@gmail.com>
2016-05-28 16:23:36 +1000

    Clear Emacs backups when emptying trash

--- aliases.sh
+++ aliases.sh
@@ -153,4 +153,4 @@ alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/La
 # Empty the Trash on all mounted volumes and the main HDD.
 # Also, clear Apple's System Logs to improve shell startup speed.
 # Finally, clear download history from quarantine. https://mths.be/bum
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; rm -rfv ~/.emacs.d/auto-save-list && mkdir ~/.emacs.d/auto-save-list; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
