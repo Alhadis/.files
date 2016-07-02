efc235641a463966d87f322059addbb60cef6ee8
Alhadis <gardnerjohng@gmail.com>
2016-07-02 17:49:05 +1000

    Update Homebrew formulae listing

--- etc/brew-list.sh
+++ etc/brew-list.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 cd $(dirname "$0")
 brew list > brew-list.txt
+
+# NOTES FOR LATER #
+# =============== #
+
+# Dependencies to prune if programs are uninstalled:
+# - Homebrew/gui/inkscape: libsigc++, cairomm, glibmm, pangomm, atkmm, gtkmm, popt
