af53f573922c58e36e221a25ffc18c08866dc2d6
Alhadis <gardnerjohng@gmail.com>
2016-08-06 23:49:16 +1000

    Dim λ to indicate a non-zero exit status

--- prompt.sh
+++ prompt.sh
@@ -1,6 +1,13 @@
 # Prompt-line customisation
-colour='\033[38;5;10m'
 
+colour='\033[38;5;10m'
+dimmed='\033[38;5;28m'
+punct='\033[38;5;22m'
+
+# Dim the λ's colour if last command exited with a non-zero status
+status-colour(){
+	[ $? -ne 0 ] && printf $dimmed
+}
 
 # Print the current branch name
 print-branch(){
@@ -10,4 +17,5 @@ print-branch(){
 	git branch --list 2>/dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ ${name}/"
 }
 
-export PS1="\[${colour}\]λ \W\[\$(print-branch)\033[38;5;22m:\033[0m\] "
+
+export PS1="\[${colour}\$(status-colour)\]λ${colour} \W\[\$(print-branch)${punct}:\033[0m\] "
