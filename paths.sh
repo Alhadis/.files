3ab9be6d065fc4650728f101d0d76a5cbd94c802
Alhadis <gardnerjohng@gmail.com>
2016-08-06 20:04:14 +1000

    Store path of Vim syntax files

--- paths.sh
+++ paths.sh
@@ -18,3 +18,4 @@ export SNIPPETS=$SNIP
 # Anything else
 export ANDROID_HOME=/usr/local/opt/android-sdk
 export GROFF_FONTS=/usr/local/Cellar/groff/1.22.3/share/groff/1.22.3/font/devps
+export VIM_SYNTAX=/usr/local/opt/vim/share/vim/vim74/syntax
