7c3c714b0e84716117d5b2efb3b0cf9e6862720f
Alhadis <gardnerjohng@gmail.com>
2016-12-15 21:33:22 +1100

    Add script to symlink NPM modules

---
+++ bin/symlink-npm.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+modules=(accordion atom-mocha basic-pagination chai-untab \
+	domtokenlist fix-ie get-options nag-window ppjson \
+	print record-jar)
+
+for i in "${modules[@]}"; do
+	target="node_modules/$i"
+	labs="$HOME/Labs"
+	
+	case $i in
+		accordion)        path="$labs/Accordion";;
+		atom-mocha)       path="$labs/Atom-Mocha";;
+		basic-pagination) path="$labs/Pagination";;
+		chai-untab)       path="$labs/Chai-Untab";;
+		domtokenlist)     path="$labs/DOMTokenList";;
+		fix-ie)           path="$labs/Fix-IE";;
+		get-options)      path="$labs/GetOptions";;
+		nag-window)       path="$labs/Nag";;
+		ppjson)           path="$labs/PPJSON";;
+		print)            path="$labs/Print";;
+		record-jar)       path="$labs/RecordJar";;
+		*)                path=;;
+	esac
+	
+	[ -d "$path" ] && [ -d "$target" ] && [ ! -L "$target" ] && {
+		rm -rf "$target"
+		ln -s "$path" "$target"
+		echo "Linked: $i -> $path"
+	}
+done;
+
+true;
