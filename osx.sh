796bfc507d2327b3f2effe72f5a32d664d4fdf1d
Alhadis <gardnerjohng@gmail.com>
2017-07-14 01:49:02 +1000

    Clean up unused or deprecated macOS preferences

--- osx.sh
+++ osx.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-# Explicitly set $EDITOR
-# See: https://github.com/VundleVim/Vundle.vim/issues/167#issuecomment-11760207
-git config --global core.editor $(which vim)
-
-
 #==============================================================================
 # Source:  https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 #==============================================================================
 
 
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
-
-
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-
-# Check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-
-# Disable Notification Center and remove the menu bar icon
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
 
 # Disable press-and-hold for keys in favour of key repeat
@@ -32,14 +15,6 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
-
-
-# Avoid creating .DS_Store files on network volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-
-# Empty trash without prompting user for confirmation
-defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 
 # Expand the following File Info panes:
@@ -56,12 +31,6 @@ defaults write com.apple.dock launchanim -bool false
 
 # Enable full keyboard access for all controls (e.g., enable Tab in modal dialogues)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-
-# Automatically open a new Finder window when a volume is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
 
 # Enable spring loading for directories, and remove the delay from doing so
@@ -83,10 +52,6 @@ defaults write com.apple.finder QuitMenuItem -bool true
 
 # Restart automatically if the computer freezes
 sudo systemsetup -setrestartfreeze on
-
-
-# Automatically quit printer app once the print jobs complete
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
 
 # Increase window resize speed for Cocoa applications
