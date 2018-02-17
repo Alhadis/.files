9ce07ce10ca276df6352c792aaf0242f2ed8dd47
Alhadis <gardnerjohng@gmail.com>
2018-02-18 05:21:00 +1100

    Update installer script to warn about Chrome freezing
    Horrible Ubuntu-specific bug. See https://askubuntu.com/q/765974/

--- install.sh
+++ install.sh
@@ -34,3 +34,24 @@ command -v npm 2>&1 >/dev/null && {
 command -v gsettings 2>&1 >/dev/null && {
 	gsettings set org.gnome.desktop.interface cursor-blink false
 }
+
+# Ubuntu: Fix Chrome launcher so it doesn't randomly freeze the system
+case `uname -s` in Linux)
+	flag=--disable-background-networking
+	launcher=/usr/share/applications/google-chrome.desktop
+	[ -f $launcher ] && ! grep -q -- $flag $launcher && {
+		printf 'Updating google-chrome launcher to use `%s`.\n' $flag
+		sudo sed -i -e "s,Exec=/usr/bin/google-chrome-stable,& $flag," $launcher &&
+		printf 'Successfully updated %s\n' $launcher
+		cat <<-EOF
+		Some additional steps also need to be performed:
+		1. In chrome://settings (under "Advanced"):
+		   - Disable "Use hardware acceleration when available"
+		   - Disable "Continue running background apps when Google Chrome is closed"
+		2. In chrome://flags:
+		   - Disable "GPU Rasterization"
+		More info: https://askubuntu.com/a/894683
+		EOF
+	}
+	unset flag launcher
+esac
