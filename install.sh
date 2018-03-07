203c11dc3addf7c3dea78df17d471256fb0e3dc6
Alhadis <gardnerjohng@gmail.com>
2018-03-07 21:59:45 +1100

    Add option to overwrite existing files when linking

--- install.sh
+++ install.sh
@@ -1,5 +1,48 @@
 #!/bin/sh
 
+#
+# install.sh: Quickly setup a new workstation
+#
+usage="${0##*/} [-h|--help] [-f|--force]"
+force=
+
+# Parse command-line switches
+while [ -n "$1" ]; do case $1 in
+
+	# Print a brief usage summary and exit
+	-h|--help|-\?)
+		printf 'Usage: %s\n' "$usage"
+		exit ;;
+
+	# Force replacement of existing files
+	-f|--force)
+		force=1 ;;
+
+	# Double-dash: Terminate option parsing
+	--)
+		shift
+		break ;;
+
+	# Invalid option: abort
+	--*|-?*)
+		>&2 printf '${0##*/}: Invalid option: "%s"\n' "$1"
+		>&2 printf 'Usage: %s\n' "$usage"
+		exit 1 ;;
+
+	# Argument not prefixed with a dash
+	*) break ;;
+
+esac; shift
+done
+
+
+# Link `$HOME/$1` to `$HOME/.files/$1` unless it's already a symlink
+symlink(){
+	[ -h $i ] && return
+	ln -sf .files/$i && printf 'Symlinked: %s -> %s\n' $i .files/$i
+}
+
+
 cd "$HOME"
 
 # Silence is golden
@@ -17,10 +60,7 @@ dotfiles='
 	.vimrc
 '
 for i in $dotfiles; do
-	[ -e $i ] || {
-		ln -s .files/$i
-		printf 'Symlinked: %s -> %s\n' $i .files/$i
-	}
+	[ -e $i ] && [ ! "$force" ] || symlink $i
 done
 unset i
 
