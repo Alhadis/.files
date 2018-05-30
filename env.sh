02cb80d1e45c21e6a44d5569135f476ef12ca18c
Alhadis <gardnerjohng@gmail.com>
2018-05-30 19:14:07 +1000

    Export install path for Documnter's Workbench 3.3

--- env.sh
+++ env.sh
@@ -2,6 +2,7 @@ PATH=/sbin:/usr/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/X11R6/bin
 PATH=~/.files/bin:$PATH:~/Mirrors/depot_tools
 export PATH
 
+export DWBHOME=/usr/local/dwb
 export GPG_TTY=`tty`
 export GREP_COLORS="sl=38;5;240:mt=1;38;5;10;48;5;22:fn=38;5;242:se=38;5;237:ln=38;5;10"
 export HISTFILE=
