5d8c7df36d422ce3e8fd09a1c38ef875e50502dc
Alhadis <gardnerjohng@gmail.com>
2018-08-03 15:06:20 +1000

    Remove launcher tampering from install script
    The phenomenon which 9ce07ce attempted to address was observed on Fedora
    with literally no Chromium-based software installed. The random freezing
    I'm witnessing on Linux must therefore be the fault of something else.

--- install.sh
+++ install.sh
@@ -167,28 +167,6 @@ command -v startxfce4 2>&1 >/dev/null && [ "$DISPLAY" ] && {
 # OS-specific configuration
 case `uname -s` in
 
-	# Ubuntu: Fix Chrome launcher so it doesn't randomly freeze the system
-	Linux)
-		flag=--disable-background-networking
-		launcher=/usr/share/applications/google-chrome.desktop
-		[ -f $launcher ] && ! grep -q -- $flag $launcher && {
-			printf 'Updating google-chrome launcher to use `%s`.\n' $flag
-			sudo sed -i -e "s,Exec=/usr/bin/google-chrome-stable,& $flag," $launcher &&
-			printf 'Successfully updated %s\n' $launcher
-			cat <<-EOF
-			Some additional steps also need to be performed:
-			1. In chrome://settings (under "Advanced"):
-			   - Disable "Use hardware acceleration when available"
-			   - Disable "Continue running background apps when Google Chrome is closed"
-			2. In chrome://flags:
-			   - Disable "GPU Rasterization"
-			More info: https://askubuntu.com/a/894683
-			EOF
-		}
-		unset flag launcher
-	;;
-
-
 	# macOS: Make Apple bloatware more tolerable
 	[Dd]arwin)
 		# Disable session saving
