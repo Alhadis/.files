a49bb7bc0e850d20f1b4decaa8446d22f95de835
Alhadis <gardnerjohng@gmail.com>
2018-09-12 04:17:26 +1000

    Define keybindings/commands to move around quicker

--- .emacs.d/keymap.el
+++ .emacs.d/keymap.el
@@ -1,6 +1,8 @@
 (global-unset-key [(control z)])
 (global-unset-key [(control x)(control z)])
 (global-unset-key [(control h)(control c)])
+(global-unset-key (kbd "M-<"))
+(global-unset-key (kbd "M->"))
 
 (global-set-key (kbd "C-h C-f") 'describe-function)
 (global-set-key (kbd "C-h C-c") 'describe-key-briefly)
@@ -8,9 +10,14 @@
 (global-set-key (kbd "<f7>") 'ispell-buffer)
 (global-set-key (kbd "C-<up>") `move-text-up)
 (global-set-key (kbd "C-<down>") `move-text-down)
-(global-set-key (kbd "C-]") 'next-buffer)
+(global-set-key (kbd "C-M-]") 'next-buffer)
 (global-set-key (kbd "C-n") 'find-file)
 (global-set-key (kbd "C-o") 'find-file-at-point)
+(global-set-key (kbd "C-<prior>") 'beginning-of-buffer)
+(global-set-key (kbd "C-<next>") 'end-of-buffer)
+(global-set-key (kbd "C-a") 'mark-whole-buffer)
+(define-key lisp-interaction-mode-map (kbd "C-e") 'eval-defun)
+
 
 ;; Custom commands
 
@@ -23,7 +30,7 @@
      (if (and (< 1 (count-windows))  ;; Otherwise, kill current buffer
               (eq major-mode 'help-mode))
          (progn (kill-buffer) (delete-window))
-         (kill-buffer)))))
+       (kill-buffer)))))
 
 (global-set-key
  (kbd "C-u")
@@ -47,3 +54,27 @@
             (not (nth 4 (syntax-ppss))))
        (describe-symbol (symbol-at-point)) ;; Test: intro(3)
        (call-interactively 'man-follow))))
+
+;;
+;; Angrier buffer switching; ignores `*Messages*' and other bullcrap.
+;;
+(global-set-key
+ (kbd "C-]")
+ (lambda ()
+  "Cycle through buffers without stopping on unwanted buffers."
+  (interactive)
+  (let (list (visible-buffer-list))
+       (setq list (remove (current-buffer) list))
+       (if (> 1 (length list))
+            (switch-to-buffer (nth 1 list))
+            (next-buffer)))))
+
+(defun visible-buffer-list ()
+  "Retrieves the contents of `buffer-list' without internal-only buffers."
+  (remove nil (mapcar (lambda (buf &optional name)
+                        (setq name (buffer-name buf))
+                        (if (or (string= " " (substring name 0 1))
+                                (string= name "*Messages*")
+                                (string= name "*Completions*"))
+                            nil buf))
+                      (buffer-list))))
