48610fe9bdf715ce3e55cdb51a36e54e7119f185
Alhadis <gardnerjohng@gmail.com>
2019-03-16 17:43:14 +1100

    Remove jarring highlights when resolving conflicts
    We're limiting this to non-graphical displays only, as the inverse-video
    effect is only used for environments with less than 88 colours.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -11,3 +11,9 @@
 (custom-set-faces
  '(font-lock-function-name-face ((t (:weight bold))))
  '(match ((t (:background "brightcyan" :foreground "black")))))
+
+;; Remove :inverse-video effect when resolving conflicts
+(unless (getenv "DISPLAY")
+  (progn (custom-set-faces
+          '(smerge-refined-added   ((t (:inherit smerge-refined-change))))
+          '(smerge-refined-removed ((t (:inherit smerge-refined-change)))))))
