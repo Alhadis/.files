a6da78dfa66e1dd2531189ca8dd232f14d864d21
Alhadis <gardnerjohng@gmail.com>
2020-04-12 10:15:22 +1000

    Export `GROFF_FONT_PATH` and `GROFF_TMAC_PATH`

--- env.sh
+++ env.sh
@@ -50,6 +50,8 @@ export DICPATH=~/.files/share/dict
 export DWBHOME=/usr/local/dwb
 export GPG_TTY=`tty`
 export GREP_COLORS="sl=38;5;240:mt=1;38;5;10;48;5;22:fn=38;5;242:se=38;5;237:ln=38;5;10"
+export GROFF_TMAC_PATH=~/.files/var/tmac:~/Labs/Mono:/usr/local/share/groff/site-tmac
+export GROFF_FONT_PATH=/usr/local/share/groff/site-font
 export HISTFILE=
 export LESS=-R
 export LYNX_CFG=~/.files/etc/lynx.cfg
