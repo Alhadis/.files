bcf832a37af883b4ee756d96ecd35b7bc8626bc8
Alhadis <gardnerjohng@gmail.com>
2021-10-15 23:33:53 +1100

    Add a faster alternative to IMDb's search bar
    This is mainly for use on mobile, where typing characters is much slower
    and every keystroke causes IMDb's stupid autocomplete "feature" to begin
    another performance-draining XMLHttpRequest.

--- etc/bookmarklets.js
+++ etc/bookmarklets.js
@@ -9,3 +9,6 @@ data:text/html;charset=UTF-8,<p></p><style>img{max-width:200px}</img>
 
 // Nuke CSS
 javascript:void(_=>{const D=document,S=D.styleSheets;while(S.length)S[0].ownerNode.remove();for(const E of D.querySelectorAll("[style]"))E.removeAttribute("style")})();
+
+// Search IMDb
+javascript:void(x=>x && (window.location = `https://imdb.com/find?q=${encodeURIComponent(x)}`))(window.prompt("Search for a movie on IMDb:"));
