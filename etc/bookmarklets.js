54523b9d654acafeb00bcfd0561760e6a5b3ee48
Alhadis <gardnerjohng@gmail.com>
2021-10-17 23:35:08 +1100

    Amend bcf832a to use `m.` subdomain when on mobile

--- etc/bookmarklets.js
+++ etc/bookmarklets.js
@@ -11,4 +11,4 @@ data:text/html;charset=UTF-8,<p></p><style>img{max-width:200px}</img>
 javascript:void(_=>{const D=document,S=D.styleSheets;while(S.length)S[0].ownerNode.remove();for(const E of D.querySelectorAll("[style]"))E.removeAttribute("style")})();
 
 // Search IMDb
-javascript:void(x=>x && (window.location = `https://imdb.com/find?q=${encodeURIComponent(x)}`))(window.prompt("Search for a movie on IMDb:"));
+javascript:void(x=>x && (window.location = `https://${screen.width <= 900 ? "m." : ""}imdb.com/find?q=${encodeURIComponent(x)}`))(window.prompt("Search for a movie on IMDb:"));
