9dda47efaaff84e1a74fa20c388834a3e6244f54
Alhadis <gardnerjohng@gmail.com>
2021-10-22 06:30:26 +1100

    Default to `-Tutf8` when formatting with groff(1)

--- env.sh
+++ env.sh
@@ -55,6 +55,7 @@ export DICPATH=~/.files/share/dict
 export DWBHOME=/usr/local/dwb
 export GPG_TTY=`tty`
 export GREP_COLORS="sl=38;5;240:mt=1;38;5;10;48;5;22:fn=38;5;242:se=38;5;237:ln=38;5;10"
+export GROFF_TYPESETTER='utf8'
 export GROFF_TMAC_PATH=~/.files/var/tmac:~/Labs/Mono:/usr/local/share/groff/site-tmac
 export GROFF_FONT_PATH=/usr/local/share/groff/site-font
 export GS_OPTIONS='-dNOSAFER -dNOSHORTERRORS -dOSTACKPRINT -dESTACKPRINT -I.'
