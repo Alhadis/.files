Alhadis/.files

commit ee1a6584b13dc3c06d7e72d9110ab3b9ecabcd58

Add global support for highlighted diff(1) output
This ensures that diff(1) and git-difftool(1) generate word-level diffs,
with the usual highlighting applied by git-diff(1). In addition, escapes
and backspaces are rendered as `ESC` and `BS`, respectively, with dimmed
text to avoid conflicting with the reverse-video effect applied by Git's
`diff-highlight` script.

aliases.sh
@@ -31,7 +31,6 @@ alias df='df -h'
 alias du='du -h'
 alias mv='mv -i'
 alias cp='cp -i'
-alias diff='diff -r'
 alias scp='scp -pr'
 
 

.gitconfig
@@ -15,6 +15,11 @@
 	undo = reset --soft HEAD~1
 [credential]
 	helper = osxkeychain
+[color]
+	pager = false
+[pager]
+	show = format-diff
+	diff = format-diff
 [diff]
 	renames = copies
 [diff "bin"]

bin/format-diff
@@ -1,0 +1,99 @@
+#!/bin/sh
+set -o pipefail >/dev/null 2>&1 || :
+set -e
+
+#
+# format-diff: Highlight diff(1) output
+#
+version='v1.0.0'
+usage="Usage:\
+	${0##*/} /path/to/diff-file
+	${0##*/} [[--] ...diff-options] file.1 file.2
+	${0##*/} [-h|--help|-?] [-v|--version]
+	diff -U4 | ${0##*/} [-]
+	git diff | ${0##*/} [-]"
+
+# Parse command-line switches
+while [ -n "$1" ]; do case $1 in
+
+	# Print a brief usage summary and exit
+	-h|--help|-\?)
+		printf '%s\n' "$usage"
+		exit ;;
+	
+	# Print a version string and exit
+	-v|--version)
+		printf '%s\n' "$version"
+		exit ;;
+
+	# Double-dash: Terminate option parsing
+	--)
+		shift
+		break ;;
+
+	# Invalid option: abort
+	--*|-?*)
+		>&2 printf '%s: Invalid option: "%s"\n' "${0##*/}" "$1"
+		>&2 printf '%s\n' "$usage"
+		exit 1 ;;
+
+	# Argument not prefixed with a dash
+	*) break ;;
+
+esac; shift
+done
+
+
+# Check dependencies are available
+have()(for i in "$@"; do command -v "$i" >/dev/null 2>&1 || return 1; done)
+
+# Command for replicating coloured git-diff(1) output
+cmd="sed '"'
+	/^commit /, /^diff / { /^diff /! s/^/[0m/; }
+	s/^[a-z][a-z]* ..*/[1m&[0m/
+	s/^--- .*/--- expected/
+	s/^+++ .*/+++ actual/
+	s/^-/[31m-/
+	s/^+/[32m+/
+	s/^\(@\{2,3\}\) .* \1/[36m&[0m/
+	s/$/[0m/
+'\'
+
+# Use less(1) for displaying output that won't fit terminal window
+if [ -t 1 ]; then
+	cmd="$cmd | less -FR"
+fi
+
+# If `diff-highlight` exists in our $PATH, assume it does what we expect
+if have diff-highlight; then
+	cmd="diff-highlight | $cmd"
+
+# Otherwise, try to locate Git's word-level diff highlighter
+elif have git realpath dirname; then
+	for path in \
+		"`command -v 2>/dev/null git | xargs realpath | xargs dirname`/../share" \
+		/usr/local/share \
+		/usr/share \
+	; do
+		path="$path/git-core/contrib/diff-highlight/diff-highlight"
+		if [ -f "$path" ] && [ -x "$path" ]; then
+			cmd="`realpath "$path"` | $cmd"
+			break
+		fi
+	done
+	unset path
+fi
+
+# Reveal backspace and escape characters in diff output
+cmd="sed 's//&[2mESC&[22m/g; s//[2mBS[22m/g' | $cmd"
+
+# 1 argument: Assume it's a file containing a unified diff
+if [ $# -eq 1 ]; then
+	[ "$1" = '-' ] && shift || cmd='cat -- "$@"'" | $cmd"
+
+# 2+ arguments: Pass ARGV to diff(1)
+elif [ $# -ge 2 ]; then
+	cmd='diff -U4 "$@"'" | $cmd"
+fi
+
+eval "$cmd"

functions.sh
@@ -24,6 +24,14 @@ visit(){
 }
 
 
+# Generate a unified diff with highlighting
+diff(){
+	set -- diff -r -U4 "$@"
+	if [ ! -t 1 ]; then command "$@"; return $?; fi
+	command "$@" | format-diff
+}
+
+
 # Run git-status(1) with an empty line inserted before each list of changes
 g(){
 	if [ ! -t 1 ]; then git status "$@"; return $?; fi

commit 1ad0c5426ede5686d4792ad38c870469dd9c4b8c

Fix missing colours in git-branch(1) output
This was caused by ee1a658's introduction of `color.pager=false`, but we
actually need this setting in order to produce legible diff-colouration.
Hence we override it for this subcommand only.

aliases.sh
@@ -10,7 +10,7 @@ alias t='run-tests'
 # Git shorthand
 have git && {
 	alias ga='git add --all'
-	alias gb='git branch -av'
+	alias gb='git branch -av --color=always'
 	alias gc='git gc && git prune -v'
 	alias gd='git diff --cached'
 	alias gh='gh-clone'