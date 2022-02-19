dee8810202a3c2a4d2996a9100943265a83014e5
Alhadis <gardnerjohng@gmail.com>
2022-02-20 08:03:10 +1100

    Add alias to call Firefox's executable directly

--- aliases.sh
+++ aliases.sh
@@ -223,6 +223,7 @@ case `uname -s` in
 		# Alias unreachable commands specific to macOS
 		alias scrub='~/.files/etc/darwin/scrub.sh'
 		alias PlistBuddy='/usr/libexec/PlistBuddy'
+		alias firefox='/Applications/Firefox.app/Contents/MacOS/firefox'
 		
 		# Mount temporary case-sensitive filesystem
 		alias tempfs='hdiutil create -size 512m -fs "Case-sensitive APFS" -type UDIF -nospotlight -attach'
