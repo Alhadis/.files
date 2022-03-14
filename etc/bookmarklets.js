ab6df55909ca5bcd9f4556185fda9b5000b96f9a
Alhadis <gardnerjohng@gmail.com>
2022-03-14 21:38:47 +1100

    Update list of browser bookmarklets

--- etc/bookmarklets.js
+++ etc/bookmarklets.js
@@ -5,10 +5,13 @@ javascript:(_=>{const D=document,L=D.location,P=L.pathname.split("/");P[4]=D.que
 javascript:void(function(){var b=document.body.parentNode;var a=b.attributes;var n="contentEditable";if(a.getNamedItem(n))a.removeNamedItem(n);else b.contentEditable=true}());
 
 // Blank
-data:text/html;charset=UTF-8,<p></p><style>img{max-width:200px}</img>
+data:text/html;charset=UTF-8,<html contenteditable%3Dtrue><style>:focus{outline:none}</style>
 
 // Nuke CSS
 javascript:void(_=>{const D=document,S=D.styleSheets;while(S.length)S[0].ownerNode.remove();for(const E of D.querySelectorAll("[style]"))E.removeAttribute("style")})();
+
+// Nuke JS
+javascript:Function.prototype.call=undefined;
 
 // Search IMDb
 javascript:void(x=>x && (window.location = `https://${screen.width <= 900 ? "m." : ""}imdb.com/find?q=${encodeURIComponent(x)}`))(window.prompt("Search for a movie on IMDb:"));
