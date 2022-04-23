ad9d5c0bbb461af056c3e9a90a6dd3f911ea4357
Alhadis <gardnerjohng@gmail.com>
2022-04-23 23:59:03 +1000

    Update en-AU and custom word dictionaries
    Also, add the custom wordlist files used by Firefox profiles and macOS's
    Spelling service to the paths that we merge/sync during an update.

--- share/dict/update.sh
+++ share/dict/update.sh
@@ -9,12 +9,14 @@ for ext in aff dic; do
 	curl -# "$url" > en_AU.$ext
 done
 
-# Keep Chrome's custom dictionary synced with ours
+# Keep custom dictionaries synced with ours
 custom=`cat custom.txt`; before="$custom"
 for path in \
+	~/"Library/Application Support/Firefox/Profiles"/*/persdict.dat \
 	~/"Library/Application Support/Google/Chrome/Custom Dictionary.txt" \
 	~/"Library/Application Support/Google/Chrome/Default/Custom Dictionary.txt" \
 	~/"Library/Spelling/LocalDictionary" \
+	~/"Library/Spelling/en_AU" \
 	~/".config/google-chrome/Default/Custom Dictionary.txt" \
 	~/".config/chromium/Default/Custom Dictionary.txt";
 do if [ -f "$path" ]; then custom="${custom}`printf '\n'; cat "${path}"`"; fi; done
