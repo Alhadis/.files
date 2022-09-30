8f01d90cb8f457add541966ca0f7b883e7b27571
Alhadis <gardnerjohng@gmail.com>
2022-10-01 03:25:35 +1000

    Ensure that `scrub.sh` truncates wiped databases
    Also ensure that ASL logs are deleted as the superuser; if any preceding
    command takes too long to finish, sudo(1) may reset its credential cache
    (causing ASL log erasure to fail with a permissions error).
    
    Also, don't delete the ~/.Trash directory proper, just its contents. Its
    deletion has been known to screw with certain programs that move deleted
    files to a holding area (like Atom).

--- etc/darwin/scrub.sh
+++ etc/darwin/scrub.sh
@@ -5,6 +5,7 @@ shopt -s dotglob
 
 #
 # scrub.sh: Wipe pointless/unused logs and junk files
+# TODO: Consider merging into function added in cc4d0fc
 #
 
 # This shouldn't happen, but better safe than sorry
@@ -17,11 +18,11 @@ fi
 sudo -v
 
 # Securely erase trash, unused logs, and autosaved/auto-generated junk
-rm -rfPv ~/.Trash /Volumes/*/.Trashes
-rm -rfPv ~/.emacs.d/auto-save-list
-rm -fPv  ~/.{bash_history,lesshst,node_repl_history,viminfo}
-rm -rfPv /private/var/log/asl/*.asl
+sudo rm -rfPv ~/.Trash/* /Volumes/*/.Trashes/*
+sudo rm -rfPv ~/.emacs.d/auto-save-list
+sudo rm -fPv  ~/.{bash_history,lesshst,node_repl_history,viminfo}
+sudo rm -rfPv /private/var/log/asl/*.asl
 
 # Keeping a log of everything I download? Creepy, Apple. Really creepy.
 # - Source: https://macgasm.net/2013/01/18/good-morning-your-mac-keeps-a-log-of-all-your-downloads/
-sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* "delete from LSQuarantineEvent";
+sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* "DELETE FROM LSQuarantineEvent; VACUUM"
